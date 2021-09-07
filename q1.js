da7b43568643b01b2a9823e77980e34db5e27343
MsDiala <diala-is-diala@hotmail.com>
2021-09-08 00:37:57 +0300

    .

--- q1.js
+++ q1.js
@@ -1,14 +1,14 @@
-function itemNumber(x,y,z){
-    
-if (x<=0 || y<=0 || z<=0){
-    retutn ("x and y and z must be positive integrs greater than 0")
-}
-else if ((40*z-800)%9===0 && (-49*z+800)%9===0){
-x = ((40*z-800)/9)+200
-y = (-49*z+800)/9
-z=z
-}
-return z ;
-}
+// function itemNumber(x,y,z){
+
+// if (x<=0 || y<=0 || z<=0){
+//     retutn ("x and y and z must be positive integrs greater than 0")
+// }
+// else if ((40*z-800)%9===0 && (-49*z+800)%9===0){
+// x = ((40*z-800)/9)+200
+// y = (-49*z+800)/9
+// z=z
+// }
+// return z ;
+// }
     
 
